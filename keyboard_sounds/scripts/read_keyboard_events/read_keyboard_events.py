152b7193ff4efdd46172cb59b61b0973b551c8b5
Francisco Javier Aceves López <francisco.aceves@wizeline.com>
2020-12-22 19:55:18 -0600

    Changed event variable name

--- keyboard_sounds/scripts/read_keyboard_events/read_keyboard_events.py
+++ keyboard_sounds/scripts/read_keyboard_events/read_keyboard_events.py
@@ -4,15 +4,15 @@ from ..config import sounds_list
 from ..config import config
 
 # this function is called whenever a key is pressed
-def _on_press(e):
+def _on_press(event):
     try:
         # constructs the file path of the sound to be played
-        sound_file = config.sound_directory + sounds_list.key_sound[e.name]
+        sound_file = config.sound_directory + sounds_list.key_sound[event.name]
         playsound(sound_file, not config.async_play)
     except OSError:
         print(f"Sound file '{sound_file}' doesn't exist")
     except KeyError:
-        print(f"'{e.name}' key not configured")
+        print(f"'{event.name}' key not configured")
 
 def read_inputs():
     # create a global listener for a KEY_DOWN event
